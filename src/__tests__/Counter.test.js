2a736d6353c2d766d56916b52ecf36f5d57534d9
Edward Suwirya <edward_suwirya@yahoo.com>
2022-01-23 10:25:29 +0700

    Give some logic counter when doing state changes

--- src/__tests__/Counter.test.js
+++ src/__tests__/Counter.test.js
@@ -13,12 +13,33 @@ describe('Counter Screen',()=>{
         const linkElement = screen.getByText(/1/i);
         expect(linkElement).toBeInTheDocument();
     });
-    //
     test('counter decrement', () => {
         render(<Counter />);
+        fireEvent.click(screen.getByText('Tambah'));
         fireEvent.click(screen.getByText('Kurang'));
-        const linkElement = screen.getByText(/-1/i);
+        const linkElement = screen.getByText(/0/i);
         expect(linkElement).toBeInTheDocument();
+    });
+    test('counter decrement show error', () => {
+        render(<Counter />);
+        fireEvent.click(screen.getByText('Kurang'));
+        //Bagaimana kalau limit bawahnya nya -1000
+        // For-Loop fireEvent ?
+        // Props initial value ?
+        const linkElement = screen.getByText(/Tidak bole negatif/i);
+        expect(linkElement).toBeInTheDocument(linkElement);
+    });
+    test('counter increment show error', () => {
+        render(<Counter />);
+        fireEvent.click(screen.getByText('Tambah'));
+        fireEvent.click(screen.getByText('Tambah'));
+        fireEvent.click(screen.getByText('Tambah'));
+        fireEvent.click(screen.getByText('Tambah'));
+        //Bagaimana kalau limit atas nya 100
+        // For-Loop fireEvent ?
+        // Props initial value ?
+        const linkElement = screen.getByText(/Tidak boleh lebih dari 3/i);
+        expect(linkElement).toBeInTheDocument(linkElement);
     });
 })
 
--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -2,14 +2,29 @@ import {useState} from "react";
 
 const Counter = ()=> {
     const [count, setCount] = useState(0);
+    const [error, setError] = useState('');
 
-    const handleIncrement = ()=>setCount(count+1)
-    const handleDecrement = ()=>setCount(count-1)
+    const handleIncrement = ()=>{
+        if(count >= 3){
+            setError('Tidak boleh lebih dari 3')
+        }else{
+            setCount(count+1)
+        }
+
+    }
+    const handleDecrement = ()=>{
+        if(count <= 0){
+            setError('Tidak bole negatif')
+        }else{
+            setCount(count-1)
+        }
+    }
     return (
         <div>
             Counter = <span id="value">{count}</span><br/>
             <button id="increment" onClick={handleIncrement}>Tambah</button>
-            <button id="decrement" onClick={handleDecrement}>Kurang</button>
+            <button id="decrement" onClick={handleDecrement}>Kurang</button><br/>
+            <span>{error}</span>
         </div>
     )
 }
